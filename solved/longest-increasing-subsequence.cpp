c03bd271bfdfb1359fc3459fd092a65d695f9dd8
brawnerq <31658792+brawnerq@users.noreply.github.com>
2020-10-29 14:25:05 -0400

    LIS nlogn solved and implemented. related problems updated

--- solved/longest-increasing-subsequence.cpp
+++ solved/longest-increasing-subsequence.cpp
@@ -1,6 +1,43 @@
 // OJ: https://leetcode.com/problems/longest-increasing-subsequence/
 // Author: https://github.com/brawnerquan
-// Time: O(n^2) can be improved to nlogn, but still working on learning how to the nlogn implementation works. will attempt to complete asap
+// Time: O(nlogn) keep track of the last element of the LIS of size z at index z of the auxillary array.
+// Space: O(n) only needed an auxillary array that could be size n at most
+};
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        if(nums.size() == 1)
+            return 1;
+        vector<int> lis;
+        vector<int>::iterator searched_index;
+        for(int i = 0; i < nums.size(); i++){
+            searched_index = lower_bound(lis.begin(), lis.end(), nums[i]);//search index returns area to consider adding to
+            //if search is at the end, nums[i] is greater than all the elements, which means we can extend the length of the longest LIS
+            if(searched_index == lis.end()){
+                lis.push_back(nums[i]);//since we are extending, we go to the lis_length - 1 + 1 index so just lis_length. then we increment
+            }else if(*searched_index == nums[i]){//if search equals the element, make sure we dont do anything
+                continue;
+            }else{//*searched_index must be greater than nums[i]. all elems before must be less than nums[i].
+                *searched_index = nums[i];//so we must replace the value as nums[i] can form an identical size LIS using one less element
+            }
+        }
+        return lis.size();
+    }
+private:
+    // void print_vector(vector<int> vec){
+    //     for(int i = 0; i < vec.size(); i++){
+    //             cout << vec[i] << ", ";
+    //     }
+    //     cout << endl;
+    // }
+};
+
+
+// OJ: https://leetcode.com/problems/longest-increasing-subsequence/
+// Author: https://github.com/brawnerquan
+// Time: O(n^2)
 // Space: O(n)
 class Solution {
 public:
@@ -27,78 +64,3 @@ public:
         return lis_length;
     }
 };
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        if(nums.size() == 0)
-            return 0;
-        if(nums.size() == 1)
-            return 1;
-        
-        vector<int> lis(nums.size(), 0);
-        int lis_length = 1;
-        int max_size = nums.size() - 1;
-        int searched_index, val_at_index;
-        for(int i = 1; i < nums.size(); i++){
-            cout << "BEFORE SEARCH" << endl;
-            searched_index = binary_search(nums, lis, max_size, 0, lis_length - 1, nums[i]);//search index returns area to consider adding to
-            cout << "AFTER SEARCH: " << searched_index <<  endl;
-            val_at_index = nums[lis[searched_index]];
-            
-
-            if(nums[i] > val_at_index){//if nums is greater, we can potentially increase the LIS at this index
-                
-                if(lis_length < searched_index + 1){//if this would make a new bigger LIS, we add it
-                    lis[searched_index + 1] = i;//store the index (index in nums) of the largest element in this new LIS
-                    ++lis_length;
-                    cout << "INCREASED LENGTH IS NOW: " << lis_length << endl;
-                }else{//otherwise, we're would be making a new one equivalent to the max or something less than the max, so we have to check
-                    if(val_at_index < nums[lis[searched_index + 1]])//if the val we're considering adding is smaller, we improve LIS so we replace
-                        lis[searched_index + 1] = i;
-                    //if not, we don't have an improvement so we just move on
-                }
-            }else if(nums[i] < val_at_index){//if nums is less than, we can improve the LIS for the size we're at so we replace
-                lis[searched_index] = i;
-                cout << "VAL_AT_INDEX: " << searched_index << "IS " << lis[searched_index] <<  endl;
-            }//if they're equal, we just continue
-            
-            // cout << "I :" << i << "SEARCH FOUND: " << searched_index << endl;            
-            cout << "AFTER ITERATION I: " << nums[i] << " LIS VECTOR LOOKS LIEK THIS:"<<  endl;
-            print_vector(lis);
-        }
-        return lis_length;
-    }
-    
-private:
-    //since we store the index of the last element in the LIS of the index size, we gotta actually check the value there
-    int binary_search(vector<int> &nums, vector<int> &arr, int max_size, int low, int high, int val){
-        int middle;
-        
-        while(low <= high){
-            middle = (low + high) / 2;
-            if(nums[arr[middle]] == val){
-                return middle;
-            }else if(nums[arr[middle]] < val){
-                low = middle + 1;
-            }else{
-                high = middle - 1;
-            }
-        }
-        if(val < nums[arr[middle]])//if the value is less than the place we ended up by
-            return max(middle - 1, 0);//make sure we don't return -1 as the index to inspect
-        else//if it's greater, make sure we dont return a value outside of the max length
-            return min(middle + 1, max_size);
-        
-        return middle;
-    }
-    
-    void print_vector(vector<int> vec){
-        for(int i = 0; i < vec.size(); i++){
-                cout << vec[i] << ", ";
-        }
-        cout << endl;
-    }
-};
-
-
--- solved/russian-doll-envelopes.cpp
+++ solved/russian-doll-envelopes.cpp
@@ -1,5 +1,48 @@
 // OJ: https://leetcode.com/problems/russian-doll-envelopes/
 // Author: https://github.com/brawnerquan
+// Time: O(nlogn) this solution is literally like 10x faster than the n^2 approach.
+// sort by width and if two elements have the same width, sort by decreasing height (so largest height is first)
+// this makes it so you can essentially do LIS in nlogn on the heights and not worry about the widths
+// Space: O(n) store the heights of the LRDE
+const int WIDTH = 0;
+const int HEIGHT = 1;
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        //sort using a lambda expression to pass in a custom sort comparator that gets the biggest to the front
+        sort(envelopes.begin(), envelopes.end(), [](vector<int> &left, vector<int> &right){if(left[WIDTH] == right[WIDTH]) return left[HEIGHT] > right[HEIGHT]; else return left[0] < right[0];});
+        vector<int> envelopes_table;
+        vector<int>::iterator search_result;
+        for(int i = 0; i < envelopes.size(); i++){
+            search_result = lower_bound(envelopes_table.begin(), envelopes_table.end(), envelopes[i][HEIGHT]);
+            //this height is the greatest in the list, so we can extend LRDE
+            if(search_result == envelopes_table.end()){
+                envelopes_table.push_back(envelopes[i][HEIGHT]);
+            }else if(*search_result == envelopes[i][HEIGHT]){//cant extend LRDE with the same height + wider or equal envelope width
+                continue;
+            }else{//we have the place of the first height that is greater than the current envelopes height
+                //we want to replace the height here because we can make an identical size LRDE with a lower height, and all following
+                //envelopes MUST have a greater width, so this LRDE at this size can be extended to make a bigger one in the end
+                *search_result = envelopes[i][HEIGHT];
+            }//you dont need to worry about replacing a LRDE with a smaller height because width is always increasing or stays the same. if its                 the same, you just replace with lower height no problem, and if the width is greater, you know any later elements widths MUST be                   greater so you dont need to worry about width and height is lower so you just replace the height!
+        }
+        return envelopes_table.size();
+    }
+private:
+//     void print_vector(vector<vector<int>> vec){
+//         for(int i = 0; i < vec.size(); i++){
+//             for(int j = 0; j < 2; j++){
+//                 cout << vec[i][j] << ", ";
+//             }
+//         }
+//         cout << endl;
+//     }
+};
+
+
+
+// OJ: https://leetcode.com/problems/russian-doll-envelopes/
+// Author: https://github.com/brawnerquan
 // Time: O(n^2). nlogn solution coming soon!
 // Space: O(n) to store an array to keep track of the the subarray solutions
 const int WIDTH = 0;
@@ -21,7 +64,7 @@ public:
             }
             num_envelopes = max(num_envelopes, envelopes_table[i]);
             // cout << "MAX_ENV FOR INDEX " << i << "is: " << envelopes_table[i] << endl;
-        } 
+        }
         return num_envelopes;
     }
 private:
